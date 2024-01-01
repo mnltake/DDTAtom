68cd9c379027bd1051f14e8a50bcf8b070e1a345
mnltake <minolu2003@ybb.ne.jp>
2024-01-01 15:03:57 +0900

    reset 後も位置を維持

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,10 @@
 #define NUM_LEDS 1
 
 uint8_t Acce = 0;    // Acceleration of motor
-uint8_t Brake_P = 0; // Brake position of motor
+uint8_t Brake_P = 0xff; // Brake position of motor
 uint8_t ID = 1;      // ID of Motor (default:1)
 uint8_t Mode = 0x03; //Angle loop
+uint8_t reset = 0;
 Receiver Receiv;
 CRGB leds[NUM_LEDS];
 // M5Stackのモジュールによって対応するRX,TXのピン番号が違うためM5製品とRS485モジュールに対応させてください
@@ -50,8 +51,9 @@ void OnDataRecv(const uint8_t *mac_addr, const uint8_t *data, int data_len) {
   snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
            mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
   Serial.print("Last Packet Recv from: "); Serial.println(macStr);
-  Serial.print("Last Packet Recv Data: "); Serial.printf("%02x %02x\n",data[0],data[1]);
+  Serial.print("Last Packet Recv Data: "); Serial.printf("%02x %02x %02x\n",data[0],data[1],data[2]);
   uint16_t newposision = data[0] | data[1]<<8;
+  reset = data[2];
   // Serial.printf("newposision: %d\n",newposision);
   uint16_t angle =  (newposision%64)*512;
   // Serial.println(angle);
@@ -66,9 +68,9 @@ void setup()
   FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS); 
   Serial.println("DDT-Motor RS485");
   // pinMode(39 ,INPUT_PULLUP);
-  delay(100);
-  motor_handler.Control_Motor(0, ID, Acce, Brake_P, &Receiv); //モータ停止
-  delay(100);
+  // delay(100);
+  // // motor_handler.Control_Motor(0, ID, Acce, Brake_P, &Receiv); //モータ停止
+  // delay(100);
   motor_handler.Set_MotorMode(Mode, ID); //モード変更
   delay(100);
     //Set device in AP mode to begin with
@@ -91,7 +93,7 @@ void loop()
     M5.update();
     leds[0] = CRGB::Red;
     FastLED.show();
-    if (M5.Btn.isPressed()){
+    if (M5.Btn.isPressed() || reset ){
       ESP.restart();
     }
     
